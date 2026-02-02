c050d35139811f4bf91cf74629f291574cad7346
Izzat888 <izzatjorayev907@gmail.com>
2026-02-02 19:28:37 +0500

    footer qismi boshlandi

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import "./App.css"
 import Hero from './assets/components/Hero/Hero'
 import Header from './assets/components/Header/Header'
 import Nav from './assets/components/Nav/Nav'
+import Footer from './assets/components/Footer.css/Footer'
 const App = () => {
   return (
     <>
       <Hero/>
       <Header/>
       <Nav/>
+      <Footer/>
     </>
   )
 }
---
+++ src/assets/components/Footer.css/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { useTranslation } from 'react-i18next'
+import "./Footer.css"
+const Footer = () => {
+    const { t } = useTranslation();
+  return (
+    <div className='footer'>
+        <div className="container">
+            <div className="footer__container">
+                <h6 className='footer__title'>{t("footer.title")}</h6>
+                <button className='footer__btn'>{t("footer.btn")}</button>
+            </div>
+            <div className='footer__box'>
+                <h6 className='footer__logo'>{t("box.title")}</h6>
+                <ul className='footer__list'>
+                    <li className='footer__item'><a className='footer__link' href="#">{t("box.link1")}</a></li>
+                    <li className='footer__item'><a className='footer__link' href="#">{t("box.link2")}</a></li>
+                    <li className='footer__item'><a className='footer__link' href="#">{t("box.link3")}</a></li>
+                </ul>
+                <p className='footer__text'>{t("box.text")}</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
